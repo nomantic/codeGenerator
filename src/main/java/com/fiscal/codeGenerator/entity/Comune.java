12678c74013e571f236c0aa7631d427188c8938d
noman <noman121998@gmail.com>
2025-01-15 12:03:08 +0100

    Added 3 new entities and 3 new repositories for the entities. with 3 new apis for a list request with service implementation. with modified application.properties for the main database

---
+++ src/main/java/com/fiscal/codeGenerator/entity/Comune.java
@@ -0,0 +1,132 @@
+package com.fiscal.codeGenerator.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "gi_comuni")
+public class Comune {
+    @Column(name = "sigla_provincia")
+    private String provinceInitials;
+    @Id
+    @Column(name = "codice_istat")
+    private String stateCode;
+    @Column(name = "denominazione_ita")
+    private String placeName;
+    @Column(name = "flag_capoluogo")
+    private String capitalCity;
+    @Column(name = "codice_belfiore")
+    private String belfireCode;
+    @Column(name = "lat")
+    private Double latitude;
+    @Column(name = "lon")
+    private Double longitude;
+    @Column(name = "superficie_kmq")
+    private Double areaDistance;
+    @Column(name = "codice_sovracomunale")
+    private String provinceCode;
+
+    public Comune() {
+    }
+
+    public Comune(String provinceInitials, String stateCode, String placeName, String capitalCity, String belfireCode, Double latitude, Double longitude, Double areaDistance, String provinceCode) {
+        this.provinceInitials = provinceInitials;
+        this.stateCode = stateCode;
+        this.placeName = placeName;
+        this.capitalCity = capitalCity;
+        this.belfireCode = belfireCode;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.areaDistance = areaDistance;
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceInitials() {
+        return provinceInitials;
+    }
+
+    public void setProvinceInitials(String provinceInitials) {
+        this.provinceInitials = provinceInitials;
+    }
+
+    public String getStateCode() {
+        return stateCode;
+    }
+
+    public void setStateCode(String stateCode) {
+        this.stateCode = stateCode;
+    }
+
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    public void setPlaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getCapitalCity() {
+        return capitalCity;
+    }
+
+    public void setCapitalCity(String capitalCity) {
+        this.capitalCity = capitalCity;
+    }
+
+    public String getBelfireCode() {
+        return belfireCode;
+    }
+
+    public void setBelfireCode(String belfireCode) {
+        this.belfireCode = belfireCode;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getAreaDistance() {
+        return areaDistance;
+    }
+
+    public void setAreaDistance(Double areaDistance) {
+        this.areaDistance = areaDistance;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Commune{" +
+                "provinceInitials='" + provinceInitials + '\'' +
+                ", stateCode='" + stateCode + '\'' +
+                ", placeName='" + placeName + '\'' +
+                ", capitalCity='" + capitalCity + '\'' +
+                ", belfireCode='" + belfireCode + '\'' +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                ", areaDistance=" + areaDistance +
+                ", provinceCode='" + provinceCode + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/fiscal/codeGenerator/service/PlaceService.java
+++ src/main/java/com/fiscal/codeGenerator/service/PlaceService.java
@@ -1,8 +1,10 @@
 package com.fiscal.codeGenerator.service;
 
-import com.fiscal.codeGenerator.entity.PlaceEntity;
-import com.fiscal.codeGenerator.entity.User;
+import com.fiscal.codeGenerator.entity.*;
+import com.fiscal.codeGenerator.repository.ComuneRepository;
+import com.fiscal.codeGenerator.repository.NationRepository;
 import com.fiscal.codeGenerator.repository.PlaceRepository;
+import com.fiscal.codeGenerator.repository.ProvinceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +15,28 @@ import java.util.List;
 @Service
 public class PlaceService implements CodeService {
     private PlaceRepository repository;
+    private NationRepository nationRepository;
+    private ProvinceRepository provinceRepository;
+    private ComuneRepository comuneRepository;
     @Autowired
-    public PlaceService(PlaceRepository repository) {
+    public PlaceService(PlaceRepository repository, NationRepository nationRepository, ProvinceRepository provinceRepository, ComuneRepository comuneRepository) {
+        this.nationRepository = nationRepository;
         this.repository = repository;
+        this.provinceRepository = provinceRepository;
+        this.comuneRepository = comuneRepository;
     }
-
+//for demo database
     public List<PlaceEntity> getPlaceList() {
         return repository.findAll();
     }
 
-    //takes a user input from the controller and sorts out the places depending on the dob of the user
+    public List<Nation> getNationList() {return nationRepository.findAll(); }
+
+    public List<Province> getProvinceList() {return provinceRepository.findAll(); }
+
+    public List<Comune> getComuneList() {return comuneRepository.findAll(); }
+
+    //takes a user input from the controller and sorts out the places depending on the dob of the user \\ for demo database
     @Override
     public List<PlaceEntity> getControlledPlaceList(User user) {
         List<PlaceEntity> controlledPlaceList = new ArrayList<>();
@@ -36,7 +50,7 @@ public class PlaceService implements CodeService {
         return controlledPlaceList;
     }
 
-    //name simplifier makes abbreviation  of a given name
+    //name simplifier makes abbreviation  of a given name \\ for demo database
     @Override
     public String nameSimplifier(User user) {
         String simplifiedName = "";
